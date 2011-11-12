e01c82bd559dcdbcc9bfb789f3e8ba92f81fbbba
Santiago Alessandri <salessandri@nasel.com.ar>
2011-11-12 19:41:55 -0300

    Fixed executable build script

--- installer/setup.py
+++ installer/setup.py
@@ -4,7 +4,7 @@ from cx_Freeze import setup, Executable
 
 copyDependentFiles=True
 silent = True
-includes = ["lxml", "lxml._elementpath", "lxml.etree", "pyreadline", "pyreadline.release"]
+includes = ["lxml", "lxml._elementpath", "lxml.etree", "pyreadline", "pyreadline.release", "gzip"]
 excludes = ["tcl", "tk", "tkinter"]
 setup(
     name = "The Mole",
